620969459caff6c6511d1c99dae7499802f4c9a5
victor alvarez <victorfelipealvarezrojas@hotmail.com>
2024-07-18 21:00:42 -0400

    add employee Entity and repository interface

---
+++ src/main/java/com/testing/sbtest/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.testing.sbtest.repository;
+
+import com.testing.sbtest.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
